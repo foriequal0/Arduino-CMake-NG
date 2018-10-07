748bd71b56391bdaec9dd615c5d8ffc3b6807d42
SeongChan Lee <foriequal@gmail.com>
2018-10-08 03:02:06 +0900

    Handles double quote, single quote

--- cmake/Platform/Properties/RecipePropertyValueResolver.cmake
+++ cmake/Platform/Properties/RecipePropertyValueResolver.cmake
@@ -16,11 +16,16 @@ endfunction()
 function(_resolve_recipe_property _property _board_id _return_var)
 
     string(REGEX MATCH "=" normal_property "${_property}")
+    message("${_property}")
+    if ("${_property}" MATCHES "^\".+\"$") # Property enclosed with qutoes
+        string(REPLACE "\"" "" _property "${_property}") # Omit quotes for now
+        set(quoted_property TRUE)
+    elseif ("${_property}" MATCHES "^'.+'$") # Property enclosed with qutoes
+        string(REPLACE "'" "" _property "${_property}") # Omit quotes for now
+        set(quoted_property TRUE)
+    endif ()
+
     if ("${normal_property}" STREQUAL "") # Recipe property, doesn't have '='
-        if ("${_property}" MATCHES "^\".+\"$") # Property enclosed with qutoes
-            string(REPLACE "\"" "" _property "${_property}") # Omit quotes for now
-            set(quoted_property TRUE)
-        endif ()
         _get_recipe_property_name("${_property}" property_name)
         _get_recipe_property_value("${_property}" property_value)
 
@@ -30,19 +35,19 @@ function(_resolve_recipe_property _property _board_id _return_var)
             set(resolved_property "${_property}")
         else ()
             _resolve_value("${property_value}" resolved_property_value "${_board_id}")
-            if (quoted_property)
-                set(resolved_property "\"${property_name}${resolved_property_value}\"")
-            else ()
-                set(resolved_property "${property_name}${resolved_property_value}")
-            endif ()
+            set(resolved_property "${property_name}${resolved_property_value}")
         endif ()
     else ()
         _get_property_name("${_property}" property_name)
         _get_property_value("${_property}" property_value)
         _resolve_value("${property_value}" resolved_property_value "${_board_id}")
+
         set(resolved_property "${property_name}=${resolved_property_value}")
     endif ()
 
+    if (quoted_property)
+        string(REPLACE ";" " " resolved_property "${resolved_property}")
+    endif ()
     # If value couldn't been resolved, return empty string
     if ("${resolved_property_value}" STREQUAL "")
         set(${_return_var} "" PARENT_SCOPE)
